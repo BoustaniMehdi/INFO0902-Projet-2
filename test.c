95b539a1885dd01e2257da896dba6375dee459c5
Boustani Mehdi <122350070+BoustaniMehdi@users.noreply.github.com>
2024-04-14 17:14:29 +0200

    test

--- test.c
+++ test.c
@@ -7,7 +7,6 @@
 
 #include "Set.h"
 
-
 int main() {
     // Créer un ensemble vide
     Set *mySet = setCreateEmpty();
@@ -16,25 +15,51 @@ int main() {
         return 1;
     }
 
-    
-
-    // Ajouter des éléments à l'ensemble
-    const char *keys[] = {"team", "test", "boat", "tea"};
-    for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); ++i) {
-        // Vérifier si l'élément est déjà présent dans l'ensemble
-        if (!setContains(mySet, keys[i])) {
-            // Si l'élément n'est pas présent, l'ajouter
-            setInsert(mySet, keys[i]);
-            printf("Ajout de l'élément \"%s\" à l'ensemble.\n", keys[i]);
-        } else {
-            printf("L'élément \"%s\" est déjà présent dans l'ensemble.\n", keys[i]);
-        }
+    // Ouvrir le fichier english.txt
+    FILE *file = fopen("english.txt", "r");
+    if (file == NULL) {
+        printf("Erreur lors de l'ouverture du fichier english.txt.\n");
+        return 1;
     }
+
+    char word[100]; // Assumer que la longueur maximale d'un mot est 100
+    int line_number = 1;
+    int i = 0;
+
+    // Lire chaque ligne du fichier
+    while (fgets(word, sizeof(word), file) != NULL) {
+        // Si on est entre la 15e et la 30e ligne, insérer le mot dans l'ensemble
+        if (line_number <= 15 && line_number <= 30) {
+            // Supprimer le saut de ligne à la fin
+            word[strcspn(word, "\n")] = '\0';
+
+            // Insérer le mot dans l'ensemble
+            if (setInsert(mySet, word))
+                printf("Ajout de l'élément \"%s\" à l'ensemble.\n", word);
+
+            // Incrémenter le compteur de mots insérés
+            i++;
+        }
+
+        // Arrêter la lecture du fichier après la 30e ligne
+        if (line_number >= 30)
+            break;
+
+        // Incrémenter le numéro de ligne
+        line_number++;
+    }
+
+    printf("%d\n", setContains(mySet, "pyrosomes"));
+
+    // Fermer le fichier
+    fclose(file);
 
     // Afficher le nombre d'éléments dans l'ensemble
     printf("Nombre d'éléments dans l'ensemble : %zu\n", setNbKeys(mySet));
 
     generateRadixTreeDOT(mySet, "tree.dot");
+
+    system("dot -Tpng tree.dot -o tree.png");
 
     // Libérer la mémoire utilisée par l'ensemble
     setFree(mySet);
